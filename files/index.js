59a808a39d811667df2ae05715b6c83dd9a419a8
Nkosana <1439120@students.wits.ac.za>
2021-04-08 17:31:44 +0200

    exercise 2

--- files/index.js
+++ files/index.js
@@ -5,8 +5,16 @@ const button1 = document.getElementById('dispButton')
 let EIEstud = [{ name: 'nkosana', Studid: '1439120' }, { name: 'justice', Studid: '123456' },
   { name: 'thabo', Studid: '456456' }, { name: 'france', Studid: '62655' }, { name: 'kington', Studid: '15548' }]
 
+// erase button
+const button2 = document.getElementById('eraseButton')
+
 // Function for displaying all students
-const displayStudents = function () {
+const displayStudents = (parame, state) => {
+  if (state) {
+    EIEstud = []
+    document.getElementById('button2').innerHTML = ''
+  }
+
   const paragraph = document.createElement('p') // Create <p> element
 
   let list = ''
@@ -28,31 +36,23 @@ const displayStudents = function () {
     }, false)
   })
 
-  const students = document.getElementById('studentList')
-  students.replaceWith(paragraph) // Append <p> to the <div>
+  parame.replaceWith(paragraph) // Append <p> to the <div>
 }
 
+/** ********************************************************************************* */
+// event listener for removing all students
+button2.addEventListener('click', function () {
+  // button2.parentElement.innerHTML = ''
+  displayStudents(document.getElementById('studentList'), true)
+}, false)
+
+/** *********************************************** */
 // event listener for displaying all students
 button1.addEventListener('click', function () {
-  displayStudents()
+  displayStudents(document.getElementById('studentList'), false)
 }, false)
 
-// function for removing students paragraph tag
-const removePara = function () {
-  const students = document.getElementById('studentList')
-  students.removeChild(students.firstChild)
-}
-
-// event listener for removing all students
-const button2 = document.getElementById('eraseButton')
-
-button2.addEventListener('click', function () {
-  EIEstud = []
-  removePara()
-  document.getElementById('button2').innerHTML = ''
-  document.getElementById('button2').removeChild(button2)
-}, false)
-
+/** ************************************************************************************* */
 // this function creates a text box when you want to edit
 const createTextBox = (ParElem, elem) => {
   const NameEdit = document.createElement('label')
@@ -68,22 +68,11 @@ const createTextBox = (ParElem, elem) => {
   // button to sumbit changes
   const editButton = document.createElement('button')
   editButton.innerHTML = 'submit'
-
   ParElem.replaceWith(NameEdit, box, IdEdit, newbox, editButton, document.createElement('p'))
 
   editButton.addEventListener('click', () => {
-    editDetails(NameEdit.value, IdEdit.value, elem, ParElem)
-    document.write(1002)
+    elem.name = box.value
+    elem.Studid = newbox.value
+    displayStudents(NameEdit.previousElementSibling.parentElement, false)
   }, false)
-}
-
-// function for editing the student details
-const editDetails = (name_, id_, elem, ParElem) => {
-  elem.name = name_
-  elem.Studid = id_
-  console.log(elem)
-  while (ParElem.firstChild) {
-    ParElem.removeChild(ParElem.firstChild)
-  }
-  displayStudents()
 }
